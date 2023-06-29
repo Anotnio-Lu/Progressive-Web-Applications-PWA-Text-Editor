6c480e862d921a43ba246aa0615921d6451edac7
Antonio lu <antonioraylu@outlook.com>
2023-06-29 23:00:32 +1000

    Plugins added and configed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,35 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }), 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'A text editor built as a PWA',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            type: "image/png",
+            fileName: 'icon_[size]x[size].png',
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
